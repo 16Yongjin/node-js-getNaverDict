e2bc507d9a9de13b90d56d34ffe9604fb2aaa063
16Yongjin <yongjin0802@gmail.com>
2018-03-08 14:07:40 +0900

    port 3333

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
 
-
 const port = process.env.PORT || 3000;
 
 var app = express();
