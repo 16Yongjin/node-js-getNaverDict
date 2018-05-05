e7d56250b7679e025ba00af4c430d864d95cbf64
16Yongjin <yongjin0802@gmail.com>
2018-05-05 19:32:12 +0900

    add cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
 
@@ -8,7 +9,8 @@ require('../model/query')
 mongoose.Promise = global.Promise
 mongoose.connect(`mongodb://localhost:27017/ptdict`)
 
-var app = express()
+const app = express()
+app.use(cors())
 require('../routes/dictRouters')(app)
 app.listen(port, () => console.log(`Server is up on port ${port}`))
 
--- test/getDict.test.js
+++ test/getDict.test.js
@@ -14,6 +14,7 @@ const { getRoot } = require('../API/GetRoot')
 
 beforeAll(async () => {
     mongoose.Promise = global.Promise
+    const a = getRoots('gostar')
     return mongoose.connect(`mongodb://localhost:27017/ptdict`)
 })
 
@@ -26,36 +27,36 @@ beforeAll(async () => {
 const firstMean = dict => dict.meanings[0].value
 
 describe('사전 API 테스트', () => {
-    test('dia', async () => {
+    xtest('dia', async () => {
         const dict = await getDict('dia')
         expect(dict.phoneticSigns).toEqual('[지아]')
     })
-    test('verb root of passaria', async () => {
+    xtest('verb root of passaria', async () => {
         const root = await getVerbRoot('passaria')
         expect(root).toEqual('passar')
     })
 
-    test('should fail on 똑똑', async () => {
+    xtest('should fail on 똑똑', async () => {
         const dict = await getKoToPtDict('똑똑')
         expect(dict).toHaveProperty('error')
     })
 
-    test('고기', async () => {
+    xtest('고기', async () => {
         const dict = await getKoToPtDict('고기')
         expect(dict.meanings[0].value).toEqual('(동물의) carne')
     })
 
-    test('Entry word 치킨', async () => {
+    xtest('Entry word 치킨', async () => {
         const dict = await getKoToPtDict('치킨')
         expect(dict.meanings[0].value).toEqual('frango')
     })
 
-    test('User entry word 안녕하세요', async () => {
+    xtest('User entry word 안녕하세요', async () => {
         const dict = await getKoToPtDict('안녕하세요')
         expect(dict.meanings[0].value).toEqual('Bom dia.')
     })
 
-    test('User entry id', async () => {
+    xtest('User entry id', async () => {
         const dict = await getKoToPtUserEntryDict('0f05a1c34c4c48cfc0e41b1237d38d35')
         expect(firstMean(dict)).toEqual('Bom dia.')
     })
@@ -72,13 +73,14 @@ describe('사전 API 테스트', () => {
         expect(firstMean(dict)).toEqual('지나가게 하다. 통과시키다. 건너 보내다. 횡단시키다.')
     })
 
-    test('should get Root of from json ', async () => {
-        const root = getRoots('passaria')
-        expect(root[0]).toEqual('passar')
-    })
-
-    test('sorria', async () => {
+    test('should get root of sorria - sorrir', async () => {
         const dict = await API('sorria')
         expect(dict.entry).toEqual('sorrir')
     })
+
+    xtest('should get Root of from json ', async () => {
+        const root = getRoots('praticado')
+        expect(root[0]).toEqual('praticar')
+    })
+
 })
